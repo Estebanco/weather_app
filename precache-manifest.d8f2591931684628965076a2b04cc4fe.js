9ecb01861bc054f28d13ac0da8d714f2de185f58
Estebanco <estecas@gmail.com>
2026-05-28 08:19:09 +0200

    Initial static app migration from Netlify to Vercel

---
+++ precache-manifest.d8f2591931684628965076a2b04cc4fe.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d49f6c89e98ef12a4db6961f16ad3b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99131d1d38da3842bc5f",
+    "url": "/static/css/2.3cecf958.chunk.css"
+  },
+  {
+    "revision": "c548a415a62c5a380f8c",
+    "url": "/static/css/main.91582aad.chunk.css"
+  },
+  {
+    "revision": "99131d1d38da3842bc5f",
+    "url": "/static/js/2.117d0d94.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.117d0d94.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c548a415a62c5a380f8c",
+    "url": "/static/js/main.28b4a34f.chunk.js"
+  },
+  {
+    "revision": "c07262949d7327fd4869",
+    "url": "/static/js/runtime-main.e39f8c0a.js"
+  },
+  {
+    "revision": "dc5d0a22d32498ca5677ac2946c0b4a2",
+    "url": "/static/media/bg.dc5d0a22.jpg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
